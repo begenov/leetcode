86917e090db3e8ed07528b8d1822af4916a35b97
Your Name <you@example.com>
2023-05-14 22:15:50 +0600

    res in Maximum Ascending Subarray

--- 1800. Maximum Ascending Subarray/main.go
+++ 1800. Maximum Ascending Subarray/main.go
@@ -8,5 +8,26 @@ func main() {
 }
 
 func maxAscendingSum(nums []int) int {
-	return 0
+	sum1, sum2 := nums[0], nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		sum1 = max(sum1, nums[i])
+
+		if nums[i] > nums[i-1] {
+			sum2 += nums[i]
+		} else {
+			sum1 = max(sum1, sum2)
+			sum2 = nums[i]
+		}
+	}
+
+	return max(sum1, sum2)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
 }
