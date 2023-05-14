0dff15b11ad4efa0556e577e6b1642368759e6eb
Your Name <you@example.com>
2023-05-14 21:53:28 +0600

    function maxAscendingSum

--- 1800. Maximum Ascending Subarray/main.go
+++ 1800. Maximum Ascending Subarray/main.go
@@ -1,1 +1,12 @@
 package main
+
+import "log"
+
+func main() {
+	input := []int{10, 20, 30, 5, 10, 50}
+	log.Println(maxAscendingSum(input))
+}
+
+func maxAscendingSum(nums []int) int {
+	return 0
+}
