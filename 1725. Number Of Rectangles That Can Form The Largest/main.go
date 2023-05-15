803bdac4978bcc0c28eea73d7d7c4de102953d44
begenov <begenovorazali@icloud.com>
2023-05-15 20:36:24 +0600

    function countGoodRectangles

--- 1725. Number Of Rectangles That Can Form The Largest/main.go
+++ 1725. Number Of Rectangles That Can Form The Largest/main.go
@@ -1,1 +1,13 @@
 package main
+
+import "log"
+
+func main() {
+	count := [][]int{{5, 8}, {3, 9}, {5, 12}, {16, 5}}
+
+	log.Println(countGoodRectangles(count))
+}
+
+func countGoodRectangles(rectangles [][]int) int {
+	return 0
+}
