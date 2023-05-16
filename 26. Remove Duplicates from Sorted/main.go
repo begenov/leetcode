bf58ed028a7407848910751e4f61fad0ef42c5b3
Your Name <you@example.com>
2023-05-16 20:53:57 +0600

    Remove Duplicates from Sorted

--- 26. Remove Duplicates from Sorted/main.go
+++ 26. Remove Duplicates from Sorted/main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	nums := []int{1, 1, 2}
+	log.Println(removeDuplicates(nums))
+}
+
+func removeDuplicates(nums []int) int {
+	var uniqueNums []int
+
+	for _, v := range nums {
+		if uniqueElements(uniqueNums, v) {
+			uniqueNums = append(uniqueNums, v)
+		}
+	}
+
+	fmt.Println(uniqueNums)
+
+	return len(uniqueNums)
+}
+
+func uniqueElements(nums []int, num int) bool {
+	for _, v := range nums {
+		if v == num {
+			return false
+		}
+	}
+	return true
+}
