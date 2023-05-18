26c87ae2cacb5f6a2246d99f20dfeceaa6099485
Your Name <you@example.com>
2023-05-18 20:25:15 +0600

    Two Sum

---
+++ 1. Two Sum/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "log"
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	log.Println(twoSum(nums, target))
+}
+
+func twoSum(nums []int, target int) []int {
+	return nil
+}
