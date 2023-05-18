9353bdcad6ace8c146c3e2669754ff62644991ec
begenov <begenovorazali@icloud.com>
2023-05-18 12:49:51 +0600

    update

--- 1561. Maximum Number of Coins You Can Get/main.go
+++ 1561. Maximum Number of Coins You Can Get/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	piles := []int{2, 4, 1, 2, 7, 8, 21, 124}
+	fmt.Println(maxCoins(piles))
+
+}
+func maxCoins(piles []int) int {
+
+	sort.Ints(piles)
+	out := 0
+	fmt.Println(piles)
+	first := 0
+	last := len(piles) - 2
+	fmt.Println(last, len(piles))
+	for first < last {
+		out += piles[last]
+		first++
+		last -= 2
+	}
+
+	return out
+}
