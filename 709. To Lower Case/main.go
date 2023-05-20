bffad459fa5a82ea9a6bd4f9ce0ebb44d8c6d359
begenov <begenovorazali@icloud.com>
2023-05-20 15:09:54 +0600

    success

--- 709. To Lower Case/main.go
+++ 709. To Lower Case/main.go
@@ -1,1 +1,21 @@
 package main
+
+import "log"
+
+func main() {
+	s := "Hello World"
+
+	log.Println(toLowerCase(s))
+}
+
+func toLowerCase(s string) string {
+	var res string
+	for _, v := range s {
+		if v >= 'A' && v <= 'Z' {
+			res += string(v + 32)
+		} else {
+			res += string(v)
+		}
+	}
+	return res
+}
