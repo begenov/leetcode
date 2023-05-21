a26183fbd09b046ba06ef97c8f6add39a7df1ea6
begenov <begenovorazali@icloud.com>
2023-05-21 07:35:12 +0600

    success

--- 941. Valid Mountain Array/main.go
+++ 941. Valid Mountain Array/main.go
@@ -8,29 +8,28 @@ func main() {
 }
 
 func validMountainArray(arr []int) bool {
-	var a []int
-	var b []int
+	if len(arr) < 3 {
+		return false
+	}
+	i := 0
 
-	for i := range arr {
-		if arr[i] < arr[i+1] {
+	for ; i < len(arr)-1; i++ {
+		if arr[i] > arr[i+1] {
 			fmt.Println(i)
-			a = append(a, arr[i])
-		} else if arr[i] == arr[i+1] {
-			return false
-		} else {
+			i++
 			break
 		}
-	}
-	for i := len(a); i < len(arr); i++ {
-		if arr[i] < arr[i+1] {
-			b = append(b, arr[i])
-		} else if arr[i] == arr[i+1] {
+		if arr[i] == arr[i+1] {
 			return false
-		} else {
-			break
 		}
 	}
 
-	fmt.Println(a, "a - b", b)
+	for ; i < len(arr); i++ {
+		if arr[i-1] <= arr[i] {
+			return false
+		}
+	}
+
 	return true
+
 }
