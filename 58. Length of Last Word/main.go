ccab4125f3f31dd8aca033613c34ef1737bd6c5d
begenov <begenovorazali@icloud.com>
2023-05-22 16:10:49 +0600

    function length of last word

--- 58. Length of Last Word/main.go
+++ 58. Length of Last Word/main.go
@@ -1,1 +1,12 @@
 package main
+
+import "fmt"
+
+func main() {
+	s := "Hello World"
+	fmt.Printf("lengthOfLastWord(s): %v\n", lengthOfLastWord(s))
+}
+func lengthOfLastWord(s string) int {
+
+	return 0
+}
