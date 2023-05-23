55996af8cca0561b92568af98228c9b796736cec
begenov <begenovorazali@icloud.com>
2023-05-23 09:05:16 +0600

    success

--- 169. Majority Element/main.go
+++ 169. Majority Element/main.go
@@ -1,1 +1,25 @@
 package main
+
+import "fmt"
+
+func main() {
+	nums := []int{2, 2, 1, 1, 1, 2, 2}
+	fmt.Printf("majorityElement(nums): %v\n", majorityElement(nums))
+
+}
+
+func majorityElement(nums []int) int {
+	m := make(map[int]int)
+	for _, v := range nums {
+		m[v]++
+	}
+	max := 0
+	k := 0
+	for key, value := range m {
+		if value > max {
+			max = value
+			k = key
+		}
+	}
+	return k
+}
