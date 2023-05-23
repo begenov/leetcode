ea421c0d3c61b1f2c2058e07b19a021747bc21cc
begenov <begenovorazali@icloud.com>
2023-05-23 09:18:57 +0600

    success

--- 268. Missing Number/main.go
+++ 268. Missing Number/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	nums := []int{3, 0, 1}
+	fmt.Printf("missingNumber(nums): %v\n", missingNumber(nums))
+}
+
+func missingNumber(nums []int) int {
+	sort.Ints(nums)
+	if nums[0] != 0 {
+		return 0
+	}
+	i, j := 0, 0
+	for ; i < len(nums)-1; i++ {
+		if nums[i]+1 != nums[i+1] {
+			j = nums[i+1] - 1
+			break
+		}
+	}
+	if j == 0 {
+		return len(nums)
+	}
+	return j
+}
