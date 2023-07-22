2b1affe8162b6d8ce345f878a587e397952c3fdc
begenov <begenovorazali@icloud.com>
2023-07-22 12:03:34 +0600

    121.\ Best\ Time\ to\ Buy\ and\ Sell\ Stock/main.go

--- 121. Best Time to Buy and Sell Stock/main.go
+++ 121. Best Time to Buy and Sell Stock/main.go
@@ -1,14 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	prices := []int{7, 1, 5, 3, 6, 4}
+	prices := []int{2, 4, 1}
 	fmt.Printf("maxProfit(prices): %v\n", maxProfit(prices))
 }
 
 func maxProfit(prices []int) int {
-
-	return 0
+	min_value := maxValue(prices)
+	max_value := 0
+	for _, v := range prices {
+		if v < min_value {
+			min_value = v
+		} else if v-min_value > max_value {
+			max_value = v - min_value
+		}
+	}
+	return max_value
 }
 
+func maxValue(prices []int) int {
+	max := 0
+	for _, v := range prices {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
